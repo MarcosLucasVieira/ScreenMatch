df3f4f9a2e955f65467674863703de91c8798762
marco <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-07-03 12:11:10 -0300

    adicionando superclass

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,10 @@
 import br.com.alura.screenmatch.model.Filme;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.Titulo;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
+        Titulo meuFilme = new Titulo();
         meuFilme.setNome(" Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMin(180);
@@ -11,6 +13,14 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpsodioPorTemporada(10);
+
 
         //System.out.println(meuFilme.somaAvalicao);
         System.out.println("Total de Avaliações: " + meuFilme.getTotalAvaliacoes());
---
+++ src/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,40 @@
+package br.com.alura.screenmatch.model;
+
+public class Serie extends Filme{
+    private int temporadas;
+    private boolean ativa;
+    private int epsodioPorTemporada;
+    private int minutosPorEpsodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpsodioPorTemporada() {
+        return epsodioPorTemporada;
+    }
+
+    public void setEpsodioPorTemporada(int epsodioPorTemporada) {
+        this.epsodioPorTemporada = epsodioPorTemporada;
+    }
+
+    public int getMinutosPorEpsodio() {
+        return minutosPorEpsodio;
+    }
+
+    public void setMinutosPorEpsodio(int minutosPorEpsodio) {
+        this.minutosPorEpsodio = minutosPorEpsodio;
+    }
+}
